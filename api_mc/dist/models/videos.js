90bf9a52066da00b67c7c35c12ec9002c4a626ca
Gustavo-DeCarli <gdecarli4@gmail.com>
2025-05-30 22:08:51 -0300

    Final commit

---
+++ api_mc/dist/models/videos.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Videos = void 0;
+const core_1 = require("./core");
+/**
+ * Model de video
+ * @type url {String} url do video
+ * @type duracao {String} tempo do video
+ */
+class Videos extends core_1.Core {
+}
+exports.Videos = Videos;
---
+++ api_mc/models/videos.ts
@@ -0,0 +1,11 @@
+import { Core } from "./core";
+
+/**
+ * Model de video
+ * @type url {String} url do video
+ * @type duracao {String} tempo do video
+ */
+export class Videos extends Core {
+  url: String;
+  duracao: String;
+}
